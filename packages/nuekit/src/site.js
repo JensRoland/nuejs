f4c94308ad908bcfa5a40f4aad88334d27e8336a
Jens Roland <jr@deepquantum.com>
2024-11-16 22:30:38 +0100

    fix: readData should handle falsy input

--- packages/nuekit/src/site.js
+++ packages/nuekit/src/site.js
@@ -42,6 +42,7 @@ export async function createSite(args) {
   }
 
   async function readData(path) {
+    if (!path) return
     try {
       const raw = await read(path)
       return yaml.load(raw)
